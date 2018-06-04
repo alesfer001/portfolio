53bcbf4c33917cc7b9569f1eb6278ef51f6b6dd0
Lesfer Ayoub <lesferayoub@gmail.com>
2018-06-04 03:17:34 +0200

    lwapp now redirects to https

--- lwapp.php
+++ lwapp.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
-
+<?
+if($_SERVER["HTTPS"] != "on"){
+    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+    exit();
+}
+?>
 <html lang="en">
 
 <head>
